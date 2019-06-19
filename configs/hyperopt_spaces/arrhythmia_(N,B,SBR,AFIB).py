01b16005d927cea2e41ed4163fd2f494fb178e42
pavelk <krylatov.pavel@gmail.com>
2019-06-19 21:38:03 +0300

    correct uasge of loguniform param

--- configs/hyperopt_spaces/arrhythmia_(N,B,SBR,AFIB).py
+++ configs/hyperopt_spaces/arrhythmia_(N,B,SBR,AFIB).py
@@ -1,10 +1,11 @@
+import math
 from hyperopt import hp
 from configs.hyperopt_spaces.base_space import BaseSpace
 
 class Space(BaseSpace):
     def _model_hparams_space(self):
         return {
-            "learning_rate": hp.loguniform("learning_rate", 0.001, 0.1)
+            "learning_rate": hp.loguniform("learning_rate", math.log(0.001), math.log(0.1))
         }
 
     def _dataset_params_space(self):
