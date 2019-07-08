5cc9525b1b9f0089fd48e1092bd32f51c6bf90f1
pavelk <krylatov.pavel@gmail.com>
2019-07-08 15:04:10 +0300

    6 classes

--- configs/hyperopt_spaces/aibolit_5layer.py
+++ configs/hyperopt_spaces/aibolit_5layer.py
@@ -5,12 +5,12 @@ from configs.hyperopt_spaces.base_space import BaseSpace
 class Space(BaseSpace):
     def _model_hparams_space(self):
         return {
-            "learning_rate": hp.loguniform("learning_rate", math.log(0.003), math.log(0.009)),
-            "dropout_rate": hp.uniform("dropout_rate", 0.3, 0.4),
+            "optimizer.params.lr": hp.loguniform("optimizer.params.lr", math.log(0.003), math.log(0.009)),
+            "dropout": hp.uniform("dropout", 0.3, 0.4),
             "filters_num": hp.quniform("filters_num", 48, 72, q=6),
             "fc_units": hp.quniform("fc_units", 30, 50, q=5),
-            "fc_layers": hp.quniform("fc_layers", 2, 3, q=1),
-            "activation_fn": hp.choice("activation_fn", ["torch.nn.ReLU", "torch.nn.ELU", "models.extensions.swish.Swish"]),
+            "fc_layers": hp.quniform("fc_layers", 2, 3, q=1)#,
+            #"activation_fn": hp.choice("activation_fn", ["torch.nn.ReLU", "torch.nn.ELU", "models.extensions.swish.Swish"]),
         }
 
     def _dataset_params_space(self):
