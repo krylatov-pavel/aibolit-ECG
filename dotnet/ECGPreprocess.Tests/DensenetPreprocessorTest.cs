912b79e83f890974f01f21ed6b704821ae9f5ec2
pavelk <krylatov.pavel@gmail.com>
2019-08-09 15:23:26 +0300

    combine transforms into densenet preprocessor

---
+++ dotnet/ECGPreprocess.Tests/DensenetPreprocessorTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Newtonsoft.Json;
+using ECGPreprocess.Preprocessors;
+
+namespace ECGPreprocess.Tests
+{
+    [TestClass]
+    public class DensenetPreprocessorTest
+    {
+        [TestMethod]
+        public void Result_Same_As_Python_Preprocessor()
+        {
+            //preprocessor includes:
+            //median filter, window 25
+            //clamp -19 .. 20
+            //scale 0 .. 5
+
+            //Arrange
+            var sourcePath = @"TestData/source_1000Hz_1s.json";
+            var targetPath = @"TestData/result_densenet_preprocessor_1000Hz_1s.json";
+            double[] source = null;
+            double[] target = null;
+
+            using (StreamReader r = new StreamReader(sourcePath))
+            {
+                string json = r.ReadToEnd();
+                source = JsonConvert.DeserializeObject<double[]>(json);
+            }
+
+            using (StreamReader r = new StreamReader(targetPath))
+            {
+                string json = r.ReadToEnd();
+                target = JsonConvert.DeserializeObject<double[]>(json);
+            }
+
+            var transform = new DensenetPreprocessor();
+
+            //Act
+            var result = transform.Transform(source);
+
+            //Assert
+            CollectionAssert.AreEqual(target, result);
+        }
+    }
+}
---
+++ dotnet/ECGPreprocessor/Preprocessors/DensenetPreprocessor.cs
@@ -0,0 +1,31 @@
+﻿using ECGPreprocess.Transforms;
+
+namespace ECGPreprocess.Preprocessors
+{
+    public class DensenetPreprocessor : ITransform1d<double>
+    {
+        private ITransform1d<double>[] transforms;
+
+        public DensenetPreprocessor()
+        {
+            this.transforms = new ITransform1d<double>[]
+            {
+                new MedianFilter1d<double>(25),
+                new Clamp1d<double>(-19.0, 21.0),
+                new Scale1d(-19.0, 21.0, 0, 5.0)
+            };
+        }
+
+        public double[] Transform(double[] x)
+        {
+            var result = x;
+
+            for(var i = 0; i < this.transforms.Length; i++)
+            {
+                result = this.transforms[i].Transform(result);
+            }
+
+            return result;
+        }
+    }
+}
